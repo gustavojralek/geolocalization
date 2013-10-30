7967b19e44d0ed382ecdd2069c015001ad00da22
Gustavo Ralek <gustavojralek@gmail.com>
2013-10-29 23:55:09 -0300

    Agregado Foundation y HTML de bienvenida

---
+++ application/views/geo/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Geolocalización</title>
+
+  
+  <link rel="stylesheet" href="resources/css/foundation.css">
+  
+
+  <script src="resources/js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+
+	<div class="row">
+		<div class="large-12 columns">
+			<h2>Bienvenido al sistema de geolocalización</h2>
+			<p>Esta es la versión 0.0.1</p>
+			<hr />
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="large-12 columns">
+
+			<form>
+			  	<fieldset>
+				    <legend>Panel</legend>
+
+				    <div class="row">
+				    	<div class="large-12 columns">
+				    		<div class="panel">
+				    			<p>Este sistema te permite encontrar la sucursal mas cercana a la dirección ingresada, mostrandote la mejor manera de llegar</p>
+				    		</div>
+				    	</div>
+				    </div>
+
+				    <div class="row">
+				      <div class="large-12 columns">
+				        <label>Ingresar dirección</label>
+				        <input type="text" placeholder="Corrientes y Florida">
+				      </div>
+				    </div>
+
+				    <div class="row">
+				      <div class="large-12 centered columns">
+				        <p><a href="#" class="success button">Procesar dirección</a></p>
+				      </div>
+				    </div>
+
+				</fieldset>
+			</form>
+			
+
+
+     
+		</div>
+
+	</div>
+
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'resources/js/vendor/zepto' : 'resources/js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="resources/js/foundation.min.js"></script>
+  <!--
+  
+  <script src="js/foundation/foundation.js"></script>
+  
+  <script src="js/foundation/foundation.interchange.js"></script>
+  
+  <script src="js/foundation/foundation.abide.js"></script>
+  
+  <script src="js/foundation/foundation.dropdown.js"></script>
+  
+  <script src="js/foundation/foundation.placeholder.js"></script>
+  
+  <script src="js/foundation/foundation.forms.js"></script>
+  
+  <script src="js/foundation/foundation.alerts.js"></script>
+  
+  <script src="js/foundation/foundation.magellan.js"></script>
+  
+  <script src="js/foundation/foundation.reveal.js"></script>
+  
+  <script src="js/foundation/foundation.tooltips.js"></script>
+  
+  <script src="js/foundation/foundation.clearing.js"></script>
+  
+  <script src="js/foundation/foundation.cookie.js"></script>
+  
+  <script src="js/foundation/foundation.joyride.js"></script>
+  
+  <script src="js/foundation/foundation.orbit.js"></script>
+  
+  <script src="js/foundation/foundation.section.js"></script>
+  
+  <script src="js/foundation/foundation.topbar.js"></script>
+  
+  -->
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
